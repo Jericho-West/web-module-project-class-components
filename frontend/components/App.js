53e1a9e56dbee2e7a192f6d37bcbcabcfaa4a8e5
Jericho-West <97463076+Jericho-West@users.noreply.github.com>
2022-05-10 20:03:01 -0400

    a

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,10 +5,12 @@ import TodoList from './TodoList';
 const todo = [
   {
     name: 'Default 1',
-    key: "1"
+    id: 1,
+    completed: false
   },
   {name: 'Default 2',
-    key: "2"
+    id: 2,
+    completed: false
   }
 ]
 
@@ -21,14 +23,32 @@ export default class App extends React.Component {
     };
   }
 
+  add = (newTodo) => {
+    //this.setState({name: this.changer, key: this.props.todo.length+1})
+    console.log("Text")
+    console.log(newTodo)
+    console.log(this.props.todo)
+    const newTodoObj = {
+      name: newTodo,
+      id: Date.now(),
+      completed: false
+    }
+    this.setState({
+      todo: [...this.state.todo, newTodoObj]
+    })
+  }
+
 
 
   render() {
     return (
       <div>
-       <Form />
        <TodoList todo={this.state.todo} />
+       <Form todo={this.state.todo} add={this.add} />
       </div>
     )
   }
 }
+
+
+//
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,12 +1,49 @@
 import React from 'react'
+import Todo from './Todo';
+
 
 export default class Form extends React.Component {
 
+  constructor(props) {
+    
+    super(props);
+    this.state = {
+    changer: ""
+    };  
+    console.log(props)
+}
+
+change = evt => {
+  const { name, value } = evt.target
+  //this.changer = {[name]: value}
+  this.setState({changer: value})
+}
+
+z = (e) => {
+  e.preventDefault()
+}
 
   render() {
+
+    console.log(this.state)
+
     return (
       <div>
-        Form
+        <label>Form</label>
+          <input
+          value = {this.state.changer}
+          type="textbox" 
+          name="name"
+          placeholder="Add To-Do"
+          onChange={this.change}
+           />
+
+
+          <button 
+          type="submit"
+          onClick={() => this.props.add(this.state.changer)}
+          onSubmit={this.z}
+          >Submit</button>
       </div>
     )
   }
